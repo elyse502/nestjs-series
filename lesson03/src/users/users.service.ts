ff49cb285aac5a75571f631356dae70a232e1789
elyse502 <elyseniyibizi502@gmail.com>
2026-07-30 19:26:28 +0200

    feat: add NotFoundException for missing user in findOne
        - Import NotFoundException from @nestjs/common
        - Throw NotFoundException with message 'User not found' when user doesn't exist
        - Provide proper HTTP 404 error response for invalid user IDs
        - Improve error handling in service layer
        - Return consistent error response for non-existent resources

--- lesson03/src/users/users.service.ts
+++ lesson03/src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
@@ -46,6 +47,8 @@ export class UsersService {
 
   findOne(id: number) {
     const user = this.users.find((user) => user.id === id);
+
+    if (!user) throw new NotFoundException('User not found');
 
     return user;
   }
